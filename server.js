76340fbd887befcefd669446b1de60f6c0f73f8c
stoncalkins@gmail.com <stoncalkins@gmail.com>
2021-12-26 13:21:30 -0500

    updated for video

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
 const sequelize = require("./config/connection");
-//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,10 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-
-// sync sequelize models to the database, then turn on the server
-
-//app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
