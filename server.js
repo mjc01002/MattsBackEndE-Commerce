21ecf8edcf576cccd7d609f215850eade6a8a3c2
stoncalkins@gmail.com <stoncalkins@gmail.com>
2021-12-26 11:08:31 -0500

    updated api routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require("./config/connection");
+//const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +13,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+
+//app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+
+
+
