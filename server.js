9ee04666e37b6055ddb4839a33f26cd4fd627f4e
stoncalkins@gmail.com <stoncalkins@gmail.com>
2021-12-26 12:17:22 -0500

    updated ready for video

--- server.js
+++ server.js
@@ -20,10 +20,6 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
-
 
 
 
